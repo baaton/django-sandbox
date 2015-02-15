6d8ae00c7290dd4681624b31594e4e514fc1ea9c
baaton <yb@chooos.com>
2015-02-15 21:08:07 +0200

    Add wysiwyg editor; update styles

--- gettingstarted/static/js/wy.js
+++ gettingstarted/static/js/wy.js
@@ -1,200 +1,294 @@
-/* http://github.com/mindmup/bootstrap-wysiwyg */
-/*global jQuery, $, FileReader*/
-/*jslint browser:true*/
-(function ($) {
-	'use strict';
-	var readFileIntoDataUrl = function (fileInfo) {
-		var loader = $.Deferred(),
-			fReader = new FileReader();
-		fReader.onload = function (e) {
-			loader.resolve(e.target.result);
-		};
-		fReader.onerror = loader.reject;
-		fReader.onprogress = loader.notify;
-		fReader.readAsDataURL(fileInfo);
-		return loader.promise();
-	};
-	$.fn.cleanHtml = function () {
-		var html = $(this).html();
-		return html && html.replace(/(<br>|\s|<div><br><\/div>|&nbsp;)*$/, '');
-	};
-	$.fn.wysiwyg = function (userOptions) {
-		var editor = this,
-			selectedRange,
-			options,
-			toolbarBtnSelector,
-			updateToolbar = function () {
-				if (options.activeToolbarClass) {
-					$(options.toolbarSelector).find(toolbarBtnSelector).each(function () {
-						var command = $(this).data(options.commandRole);
-						if (document.queryCommandState(command)) {
-							$(this).addClass(options.activeToolbarClass);
-						} else {
-							$(this).removeClass(options.activeToolbarClass);
-						}
-					});
-				}
-			},
-			execCommand = function (commandWithArgs, valueArg) {
-				var commandArr = commandWithArgs.split(' '),
-					command = commandArr.shift(),
-					args = commandArr.join(' ') + (valueArg || '');
-				document.execCommand(command, 0, args);
-				updateToolbar();
-			},
-			bindHotkeys = function (hotKeys) {
-				$.each(hotKeys, function (hotkey, command) {
-					editor.keydown(hotkey, function (e) {
-						if (editor.attr('contenteditable') && editor.is(':visible')) {
-							e.preventDefault();
-							e.stopPropagation();
-							execCommand(command);
-						}
-					}).keyup(hotkey, function (e) {
-						if (editor.attr('contenteditable') && editor.is(':visible')) {
-							e.preventDefault();
-							e.stopPropagation();
-						}
-					});
-				});
-			},
-			getCurrentRange = function () {
-				var sel = window.getSelection();
-				if (sel.getRangeAt && sel.rangeCount) {
-					return sel.getRangeAt(0);
-				}
-			},
-			saveSelection = function () {
-				selectedRange = getCurrentRange();
-			},
-			restoreSelection = function () {
-				var selection = window.getSelection();
-				if (selectedRange) {
-					try {
-						selection.removeAllRanges();
-					} catch (ex) {
-						document.body.createTextRange().select();
-						document.selection.empty();
-					}
+!function($, wysi) {
+    "use strict";
 
-					selection.addRange(selectedRange);
-				}
-			},
-			insertFiles = function (files) {
-				editor.focus();
-				$.each(files, function (idx, fileInfo) {
-					if (/^image\//.test(fileInfo.type)) {
-						$.when(readFileIntoDataUrl(fileInfo)).done(function (dataUrl) {
-							execCommand('insertimage', dataUrl);
-						}).fail(function (e) {
-							options.fileUploadError("file-reader", e);
-						});
-					} else {
-						options.fileUploadError("unsupported-file-type", fileInfo.type);
-					}
-				});
-			},
-			markSelection = function (input, color) {
-				restoreSelection();
-				if (document.queryCommandSupported('hiliteColor')) {
-					document.execCommand('hiliteColor', 0, color || 'transparent');
-				}
-				saveSelection();
-				input.data(options.selectionMarker, color);
-			},
-			bindToolbar = function (toolbar, options) {
-				toolbar.find(toolbarBtnSelector).click(function () {
-					restoreSelection();
-					editor.focus();
-					execCommand($(this).data(options.commandRole));
-					saveSelection();
-				});
-				toolbar.find('[data-toggle=dropdown]').click(restoreSelection);
+    var templates = {
+        "font-styles": "<li class='dropdown'>" +
+                           "<a class='btn dropdown-toggle' data-toggle='dropdown' href='#'>" +
+                               "<i class='icon-font'></i>&nbsp;<span class='current-font'>Normal text</span>&nbsp;<b class='caret'></b>" +
+                           "</a>" +
+                           "<ul class='dropdown-menu'>" +
+                               "<li><a data-wysihtml5-command='formatBlock' data-wysihtml5-command-value='div'>Normal text</a></li>" +
+                               "<li><a data-wysihtml5-command='formatBlock' data-wysihtml5-command-value='h1'>Heading 1</a></li>" +
+                               "<li><a data-wysihtml5-command='formatBlock' data-wysihtml5-command-value='h2'>Heading 2</a></li>" +
+                           "</ul>" +
+                       "</li>",
+        "emphasis":    "<li>" +
+                           "<div class='btn-group'>" +
+                               "<a class='btn' data-wysihtml5-command='bold' title='CTRL+B'>Bold</a>" +
+                               "<a class='btn' data-wysihtml5-command='italic' title='CTRL+I'>Italic</a>" +
+                               "<a class='btn' data-wysihtml5-command='underline' title='CTRL+U'>Underline</a>" +
+                           "</div>" +
+                       "</li>",
+        "lists":       "<li>" +
+                           "<div class='btn-group'>" +
+                               "<a class='btn' data-wysihtml5-command='insertUnorderedList' title='Unordered List'><i class='icon-list'></i></a>" +
+                               "<a class='btn' data-wysihtml5-command='insertOrderedList' title='Ordered List'><i class='icon-th-list'></i></a>" +
+                               "<a class='btn' data-wysihtml5-command='Outdent' title='Outdent'><i class='icon-indent-right'></i></a>" +
+                               "<a class='btn' data-wysihtml5-command='Indent' title='Indent'><i class='icon-indent-left'></i></a>" +
+                           "</div>" +
+                       "</li>",
+        "link":        "<li>" +
+                           "<div class='bootstrap-wysihtml5-insert-link-modal modal hide fade'>" +
+                               "<div class='modal-header'>" +
+                                   "<a class='close' data-dismiss='modal'>&times;</a>" +
+                                   "<h3>Insert Link</h3>" +
+                               "</div>" +
+                               "<div class='modal-body'>" +
+                                   "<input value='http://' class='bootstrap-wysihtml5-insert-link-url input-xlarge'>" +
+                               "</div>" +
+                               "<div class='modal-footer'>" +
+                                   "<a href='#' class='btn' data-dismiss='modal'>Cancel</a>" +
+                                   "<a href='#' class='btn btn-primary' data-dismiss='modal'>Insert link</a>" +
+                               "</div>" +
+                           "</div>" +
+                           "<a class='btn' data-wysihtml5-command='createLink' title='Link'><i class='icon-share'></i></a>" +
+                       "</li>",
+        "image":       "<li>" +
+                           "<div class='bootstrap-wysihtml5-insert-image-modal modal hide fade'>" +
+                               "<div class='modal-header'>" +
+                                   "<a class='close' data-dismiss='modal'>&times;</a>" +
+                                   "<h3>Insert Image</h3>" +
+                               "</div>" +
+                               "<div class='modal-body'>" +
+                                   "<input value='http://' class='bootstrap-wysihtml5-insert-image-url input-xlarge'>" +
+                               "</div>" +
+                               "<div class='modal-footer'>" +
+                                   "<a href='#' class='btn' data-dismiss='modal'>Cancel</a>" +
+                                   "<a href='#' class='btn btn-primary' data-dismiss='modal'>Insert image</a>" +
+                               "</div>" +
+                           "</div>" +
+                           "<a class='btn' data-wysihtml5-command='insertImage' title='Insert image'><i class='icon-picture'></i></a>" +
+                       "</li>",
 
-				toolbar.find('input[type=text][data-' + options.commandRole + ']').on('webkitspeechchange change', function () {
-					var newValue = this.value; /* ugly but prevents fake double-calls due to selection restoration */
-					this.value = '';
-					restoreSelection();
-					if (newValue) {
-						editor.focus();
-						execCommand($(this).data(options.commandRole), newValue);
-					}
-					saveSelection();
-				}).on('focus', function () {
-					var input = $(this);
-					if (!input.data(options.selectionMarker)) {
-						markSelection(input, options.selectionColor);
-						input.focus();
-					}
-				}).on('blur', function () {
-					var input = $(this);
-					if (input.data(options.selectionMarker)) {
-						markSelection(input, false);
-					}
+        "html":
+                       "<li>" +
+                           "<div class='btn-group'>" +
+                               "<a class='btn' data-wysihtml5-action='change_view' title='Edit HTML'><i class='icon-pencil'></i></a>" +
+                           "</div>" +
+                       "</li>"
+    };
+
+    var defaultOptions = {
+        "font-styles": true,
+        "emphasis": true,
+        "lists": true,
+        "html": false,
+        "link": true,
+        "image": true,
+        events: {},
+        parserRules: {
+            tags: {
+                "b":  {},
+                "i":  {},
+                "br": {},
+                "ol": {},
+                "ul": {},
+                "li": {},
+                "h1": {},
+                "h2": {},
+                "u": 1,
+                "img": {
+                    "check_attributes": {
+                        "width": "numbers",
+                        "alt": "alt",
+                        "src": "url",
+                        "height": "numbers"
+                    }
+                },
+                "a":  {
+                    set_attributes: {
+                        target: "_blank",
+                        rel:    "nofollow"
+                    },
+                    check_attributes: {
+                        href:   "url" // important to avoid XSS
+                    }
+                }
+            }
+        },
+        stylesheets: []
+    };
+
+    var Wysihtml5 = function(el, options) {
+        this.el = el;
+        this.toolbar = this.createToolbar(el, options || defaultOptions);
+        this.editor =  this.createEditor(options);
+
+        window.editor = this.editor;
+
+        $('iframe.wysihtml5-sandbox').each(function(i, el){
+            $(el.contentWindow).off('focus.wysihtml5').on({
+              'focus.wysihtml5' : function(){
+                 $('li.dropdown').removeClass('open');
+               }
+            });
+        });
+    };
+
+    Wysihtml5.prototype = {
+
+        constructor: Wysihtml5,
+
+        createEditor: function(options) {
+            options = $.extend(defaultOptions, options || {});
+		    options.toolbar = this.toolbar[0];
+
+		    var editor = new wysi.Editor(this.el[0], options);
+
+            if(options && options.events) {
+                for(var eventName in options.events) {
+                    editor.on(eventName, options.events[eventName]);
+                }
+            }
+
+            return editor;
+        },
+
+        createToolbar: function(el, options) {
+            var self = this;
+            var toolbar = $("<ul/>", {
+                'class' : "wysihtml5-toolbar",
+                'style': "display:none"
+            });
+
+            for(var key in defaultOptions) {
+                var value = false;
+
+                if(options[key] !== undefined) {
+                    if(options[key] === true) {
+                        value = true;
+                    }
+                } else {
+                    value = defaultOptions[key];
+                }
+
+                if(value === true) {
+                    toolbar.append(templates[key]);
+
+                    if(key == "html") {
+                        this.initHtml(toolbar);
+                    }
+
+                    if(key == "link") {
+                        this.initInsertLink(toolbar);
+                    }
+
+                    if(key == "image") {
+                        this.initInsertImage(toolbar);
+                    }
+                }
+            }
+
+            toolbar.find("a[data-wysihtml5-command='formatBlock']").click(function(e) {
+                var el = $(e.srcElement);
+                self.toolbar.find('.current-font').text(el.html());
+            });
+
+            this.el.before(toolbar);
+
+            return toolbar;
+        },
+
+        initHtml: function(toolbar) {
+            var changeViewSelector = "a[data-wysihtml5-action='change_view']";
+            toolbar.find(changeViewSelector).click(function(e) {
+                toolbar.find('a.btn').not(changeViewSelector).toggleClass('disabled');
+            });
+        },
+
+        initInsertImage: function(toolbar) {
+            var self = this;
+            var insertImageModal = toolbar.find('.bootstrap-wysihtml5-insert-image-modal');
+            var urlInput = insertImageModal.find('.bootstrap-wysihtml5-insert-image-url');
+            var insertButton = insertImageModal.find('a.btn-primary');
+            var initialValue = urlInput.val();
+
+            var insertImage = function() {
+                var url = urlInput.val();
+                urlInput.val(initialValue);
+                self.editor.composer.commands.exec("insertImage", url);
+            };
+
+            urlInput.keypress(function(e) {
+                if(e.which == 13) {
+                    insertImage();
+                    insertImageModal.modal('hide');
+                }
+            });
+
+            insertButton.click(insertImage);
+
+            insertImageModal.on('shown', function() {
+                urlInput.focus();
+            });
+
+            insertImageModal.on('hide', function() {
+                self.editor.currentView.element.focus();
+            });
+
+            toolbar.find('a[data-wysihtml5-command=insertImage]').click(function() {
+                insertImageModal.modal('show');
+                insertImageModal.on('click.dismiss.modal', '[data-dismiss="modal"]', function(e) {
+					e.stopPropagation();
 				});
-				toolbar.find('input[type=file][data-' + options.commandRole + ']').change(function () {
-					restoreSelection();
-					if (this.type === 'file' && this.files && this.files.length > 0) {
-						insertFiles(this.files);
-					}
-					saveSelection();
-					this.value = '';
+                return false;
+            });
+        },
+
+        initInsertLink: function(toolbar) {
+            var self = this;
+            var insertLinkModal = toolbar.find('.bootstrap-wysihtml5-insert-link-modal');
+            var urlInput = insertLinkModal.find('.bootstrap-wysihtml5-insert-link-url');
+            var insertButton = insertLinkModal.find('a.btn-primary');
+            var initialValue = urlInput.val();
+
+            var insertLink = function() {
+                var url = urlInput.val();
+                urlInput.val(initialValue);
+                self.editor.composer.commands.exec("createLink", {
+                    href: url,
+                    target: "_blank",
+                    rel: "nofollow"
+                });
+            };
+            var pressedEnter = false;
+
+            urlInput.keypress(function(e) {
+                if(e.which == 13) {
+                    insertLink();
+                    insertLinkModal.modal('hide');
+                }
+            });
+
+            insertButton.click(insertLink);
+
+            insertLinkModal.on('shown', function() {
+                urlInput.focus();
+            });
+
+            insertLinkModal.on('hide', function() {
+                self.editor.currentView.element.focus();
+            });
+
+            toolbar.find('a[data-wysihtml5-command=createLink]').click(function() {
+                insertLinkModal.modal('show');
+                insertLinkModal.on('click.dismiss.modal', '[data-dismiss="modal"]', function(e) {
+					e.stopPropagation();
 				});
-			},
-			initFileDrops = function () {
-				editor.on('dragenter dragover', false)
-					.on('drop', function (e) {
-						var dataTransfer = e.originalEvent.dataTransfer;
-						e.stopPropagation();
-						e.preventDefault();
-						if (dataTransfer && dataTransfer.files && dataTransfer.files.length > 0) {
-							insertFiles(dataTransfer.files);
-						}
-					});
-			};
-		options = $.extend({}, $.fn.wysiwyg.defaults, userOptions);
-		toolbarBtnSelector = 'a[data-' + options.commandRole + '],button[data-' + options.commandRole + '],input[type=button][data-' + options.commandRole + ']';
-		bindHotkeys(options.hotKeys);
-		if (options.dragAndDropImages) {
-			initFileDrops();
-		}
-		bindToolbar($(options.toolbarSelector), options);
-		editor.attr('contenteditable', true)
-			.on('mouseup keyup mouseout', function () {
-				saveSelection();
-				updateToolbar();
-			});
-		$(window).bind('touchend', function (e) {
-			var isInside = (editor.is(e.target) || editor.has(e.target).length > 0),
-				currentRange = getCurrentRange(),
-				clear = currentRange && (currentRange.startContainer === currentRange.endContainer && currentRange.startOffset === currentRange.endOffset);
-			if (!clear || isInside) {
-				saveSelection();
-				updateToolbar();
-			}
-		});
-		return this;
-	};
-	$.fn.wysiwyg.defaults = {
-		hotKeys: {
-			'ctrl+b meta+b': 'bold',
-			'ctrl+i meta+i': 'italic',
-			'ctrl+u meta+u': 'underline',
-			'ctrl+z meta+z': 'undo',
-			'ctrl+y meta+y meta+shift+z': 'redo',
-			'ctrl+l meta+l': 'justifyleft',
-			'ctrl+r meta+r': 'justifyright',
-			'ctrl+e meta+e': 'justifycenter',
-			'ctrl+j meta+j': 'justifyfull',
-			'shift+tab': 'outdent',
-			'tab': 'indent'
-		},
-		toolbarSelector: '[data-role=editor-toolbar]',
-		commandRole: 'edit',
-		activeToolbarClass: 'btn-info',
-		selectionMarker: 'edit-focus-marker',
-		selectionColor: 'darkgrey',
-		dragAndDropImages: true,
-		fileUploadError: function (reason, detail) { console.log("File upload error", reason, detail); }
-	};
-}(window.jQuery));
+                return false;
+            });
+
+
+        }
+    };
+
+    $.fn.wysihtml5 = function (options) {
+        return this.each(function () {
+            var $this = $(this);
+            $this.data('wysihtml5', new Wysihtml5($this, options));
+        });
+    };
+
+    $.fn.wysihtml5.Constructor = Wysihtml5;
+
+}(window.jQuery, window.wysihtml5);
--- hello/forms.py
+++ hello/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 
 class AddPost(forms.Form):
-    title = forms.CharField(label='Article title', max_length=50)
-    body = forms.CharField(label='Your Article', widget=forms.Textarea(attrs={'id': 'editor'}))
+    title = forms.CharField(label='Article title', max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    body = forms.CharField(label='Your Article', widget=forms.Textarea(attrs={'id': 'editor', 'class': 'form-textarea form-control'}))
 
 class AddComment(forms.Form):
-    user_name = forms.CharField(label='Your Name', max_length=250)
-    body = forms.CharField(label='Your Comment', widget=forms.Textarea())
+    user_name = forms.CharField(label='Your Name', max_length=250, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    body = forms.CharField(label='Your Comment', widget=forms.Textarea(attrs={'class': 'form-control form-comment'}))
